20c5216e605483921e1e641f5395d7b0f2e0c367
BXYMartin <BXYMartin@hotmail.com>
2019-12-03 12:40:01 +0800

    add test unit

--- setup.py
+++ setup.py
@@ -1,4 +1,35 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test
+import os
+import sys
+import unittest
+def discover_and_run_tests():
+    # get setup.py directory
+    setup_file = sys.modules['__main__'].__file__
+    setup_dir = os.path.abspath(os.path.dirname(setup_file))
+
+    # use the default shared TestLoader instance
+    test_loader = unittest.defaultTestLoader
+
+    # use the basic test runner that outputs to sys.stderr
+    test_runner = unittest.TextTestRunner()
+    print(os.path.join(setup_dir, "test"))
+
+    # automatically discover all tests
+    test_suite = test_loader.discover(os.path.join(setup_dir, "test"), pattern='test_*.py')
+    print(test_suite)
+
+    # run the test suite
+    test_runner.run(test_suite)
+
+class DiscoverTest(test):
+    def finalize_options(self):
+        test.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        discover_and_run_tests()
 
 setup(
     name = "MERRA2_CSM",
@@ -12,7 +43,7 @@ setup(
     author = "Jamie Bright, Yue Zhang, Martin Bai",
     author_email = "Not Available",
 
-    packages = find_packages(),
+    packages = find_packages(exclude=['test']),
     include_package_data = True,
     platforms = "any",
     install_requires = [
@@ -30,6 +61,7 @@ setup(
         ],
 
     scripts = [],
+    cmdclass = {'test': DiscoverTest},
     entry_points = {
         'console_scripts': [
             'merra2_downloader = MERRA2_CSM.downloader.socket:main'
---
+++ test/test_downloader.py
@@ -0,0 +1,112 @@
+from multiprocessing import freeze_support
+import clearskypy
+import unittest
+import os
+
+class test_downloader(unittest.TestCase):
+    def setUp(self):
+        # SIMPLE USE CONFIGURATION
+        ## The user should define the input parameters in this script for the easiest use case of the package
+
+        # specify your user authentication
+        self.username = input("Please Enter Your USERNAME: ")
+        self.password = input("Please Enter Your PASSWORD: ")
+
+        # specify the date range
+        self.initial_year = 2018
+        self.initial_month = 1
+        self.initial_day = 1
+
+        # specify the end date
+        self.final_year = 2018
+        self.final_month = 1
+        self.final_day = 1
+
+        # specify the bottom left corner of the rectangular region
+        self.lat_1 = -90
+        self.lon_1 = -180
+
+        # specify the top left corner of the rectangular region
+        self.lat_2 = 90
+        self.lon_2 = 180
+
+        # specify the number of threads, this is the number of instances that will run at the same time
+        # theoretically, this can be as many as you think your machine can handle, though GESDISC may
+        # impose a limit
+        self.thread_num = 20
+
+        # specify the number of connections to each file
+        # we know that GESDISC does not allow too many connections, we recommend leaving at 1.
+        self.connection_num = 1
+
+        # specify whether or not to delete the temporary data [True/False]
+        self.delete_temp_dir = True
+
+        # specify whether to print more detail to the console
+        self.verbose = True
+
+        # specify the output destination of the data, depending on operating system changes the file separator
+        # default location is where you run the script, specify here if you want to change it using os.join.
+        self.output_dir = os.path.join(os.getcwd(),"MERRA2_data")
+
+        # Here you can change what actually gets downloaded
+        # full list of variables can be founnd here https://gmao.gsfc.nasa.gov/pubs/docs/Bosilovich785.pdf
+        self.merra2_var_dicts = {
+                "rad": {
+                    "esdt_dir": "M2T1NXRAD.5.12.4",
+                    "collection": "tavg1_2d_rad_Nx",
+                    "merra_name": ["ALBEDO", "CLDTOT", "SWGDN", "SWGDNCLR", "TAUTOT"],
+                    "standard_name": "radiation",
+                    "least_significant_digit": 3,
+
+                    },
+                "slv": {
+                    "esdt_dir": "M2T1NXSLV.5.12.4",
+                    "collection": "tavg1_2d_slv_Nx",
+                    "merra_name": ["TQV", "TO3", "PS", "CLDPRS", "CLDTMP", "H1000", "H250", "H500", "H850", "T2M", "U250", "U500", "V250", "V500"],
+                    "standard_name": "surface",
+                    "least_significant_digit": 3,
+
+                    },
+                "aer": {
+                    "esdt_dir": "M2T1NXAER.5.12.4",
+                    "collection": "tavg1_2d_aer_Nx",
+                    "merra_name": ["TOTSCATAU", "TOTEXTTAU", "TOTANGSTR"],
+                    "standard_name": "aerosols",
+                    "least_significant_digit": 3,
+
+                    },
+        }
+
+    def test_download(self):
+        clearskypy.downloader.run(
+            auth={"uid": self.username, "password": self.password},
+            initial_year=self.initial_year,
+            final_year=self.final_year,
+            initial_month=self.initial_month,
+            final_month=self.final_month,
+            initial_day=self.initial_day,
+            final_day=self.final_day,
+            lat_1=self.lat_1,
+            lat_2=self.lat_2,
+            lon_1=self.lon_1,
+            lon_2=self.lon_2,
+            delete_temp_dir=self.delete_temp_dir,
+            verbose=self.verbose,
+            thread_num=self.thread_num,
+            connection_num=self.connection_num,
+            merra2_var_dicts=self.merra2_var_dicts,
+            output_dir=self.output_dir
+        )
+
+    def tearDown(self):
+        pass
+
+
+
+# RUN THE DOWNLOADER
+if __name__ == "__main__":
+    freeze_support()
+    unittest.main()
+
+
