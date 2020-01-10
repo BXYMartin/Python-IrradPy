f1f64140fa1fdb2b0f931911a1230c7eb8fd122c
Jamie M. Bright <serjmb@nus.edu.sg>
2020-01-10 12:33:39 +0800

    combined examples into one script. modified inputs

--- example/example_clearsky.py
+++ example/example_clearsky.py
@@ -6,19 +6,18 @@ from matplotlib import pyplot as plt
 if __name__ == '__main__':
     # set some example latitudes, longitudes and elevations
     # latitudes range from -90 (south pole) to +90 (north pole) in degrees
-    latitudes = np.array([1.300350, 39.976060,32.881034])
+    latitudes = np.array([[1.300341, 39.97937]])
     # longitudes range from -180 (west) through 0 at prime meridian to +180 (east)
-    longitudes = np.array([103.771630, 116.344477, -117.233575])
+    longitudes = np.array([[103.771663, 116.34653]]) 
     # elevations are in metres, this influences some solar elevation angles and scale height corrections
-    elevations = np.array([30, 50.2, 62])
-     
+    elevations = np.array([[43, 53]])
+
     # set the time series that you wish to model. Thi can be unique per locaton.
     # first, specify the temporal resolution in minutes
     time_delta = 10  # minute
     # timedef is a list of [(start time , end time)] for each location defined. 
-    timedef = [('2010-01-01T00:15:00', '2010-01-01T23:45:00'), 
-               ('2010-06-01T00:15:00', '2010-06-01T23:45:00'),
-               ('2010-09-01T00:15:00', '2010-09-01T23:45:00')]
+    timedef = [('2010-01-01T06:15:00', '2010-01-02T04:45:00'), 
+               ('2010-09-01T08:15:00', '2010-09-02T04:45:00')]  
     # use timeseries_builder to build time series for different station
     time = clearskypy.model.timeseries_builder(timedef, time_delta)
 
@@ -28,7 +27,14 @@ if __name__ == '__main__':
     # build the clear-sky REST2v5 model object
     test_rest2 = clearskypy.model.ClearSkyREST2v5(latitudes, longitudes, elevations, time, dataset_dir)
     # run the REST2v5 clear-sky model
-    [ghics, dnics, difcs] = test_rest2.REST2v5()
+    [ghics_rest2, dnics_rest2, difcs_rest2] = test_rest2.REST2v5()
+    
+    # create the MAC2 model class object
+    test_mac = clearskypy.model.ClearSkyMAC2(latitudes, longitudes, elevations, time, dataset_dir)
+    # run the MAC2 model
+    [ghics_mac2, dnics_mac2, difcs_mac2] = test_mac.MAC2()
+
+
 
     plt.figure(1)
 
